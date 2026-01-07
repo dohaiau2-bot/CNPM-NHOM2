af4bfe4c19e41167b1b9ad65dca03552735c81cf
Your Name <your_email@example.com>
2026-01-07 21:11:06 +0700

    update uncessary

--- delete_book_MVC/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-models/__init__.py
-Package chứa các Model xử lý dữ liệu
-"""
-
-from models.database import Database
-from models.book_model import BookModel
-
-__all__ = ['Database', 'BookModel']
